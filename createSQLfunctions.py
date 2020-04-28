47c1a97e4af2e49262a26729a6b6d4423a15fe12
Rajesh <rajeshpachaikani@live.in>
2020-04-28 13:46:25 +0000

    SQL functions Updated

---
+++ createSQLfunctions.py
@@ -0,0 +1,43 @@
+import psycopg2
+
+try:
+    con = psycopg2.connect(
+        host = "localhost",
+        database='postgres',
+        user='postgres',
+        password='OpenCV'
+    )
+except:
+    print('connection failed!')
+    con = None
+
+cur = con.cursor()
+
+cur.execute("""
+CREATE OR REPLACE FUNCTION public.vec_sub(arr1 double precision[], arr2 double precision[])
+ RETURNS double precision[]
+ LANGUAGE sql
+ STRICT
+AS $function$	
+SELECT array_agg (result)
+    FROM (SELECT (tuple.val1 - tuple.val2) * (tuple.val1 - tuple.val2)
+        AS result
+        FROM (SELECT UNNEST ($1) AS val1
+               , UNNEST ($2) AS val2
+               , generate_subscripts ($1, 1) AS ix) tuple
+    ORDER BY ix) inn;
+	
+	$function$
+
+""")
+cur.execute('''
+CREATE OR REPLACE FUNCTION public.euclidian(arr1 double precision[], arr2 double precision[])
+ RETURNS double precision
+ LANGUAGE sql
+AS $function$
+select sqrt (SUM (tab.v)) as euclidian from (SELECT
+     UNNEST (vec_sub (arr1, arr2)) as v) as tab;
+	 $function$
+
+''')
+con.commit()
